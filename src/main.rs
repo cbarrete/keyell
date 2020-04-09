5faf0ec4ddbd0d20027201ef9c1f1f30a6fe7b0a
Cédric Barreteau <cbarrete@users.noreply.github.com>
2020-04-10 00:12:08 +0200

    Basic raytracer

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ mod types;
 mod ppm_writer;
 
 use crate::ppm_writer::PPMWriter;
-use crate::types::Vec3;
+use crate::types::{Ray, Vec3};
+
+// blend blue and white, used to make a gradient background.
+fn ray_color(ray: &Ray) -> Vec3 {
+    let t = 0.5 * (ray.direction.unit().y + 1.);
+    t * Vec3::new(0.5, 0.7, 1.0) + (1. - t) * Vec3::new(1., 1., 1.)
+}
 
 fn main() -> Result<(), std::io::Error> {
     let width  = 200;
@@ -13,12 +19,18 @@ fn main() -> Result<(), std::io::Error> {
     let mut writer = PPMWriter::new(File::create("out.ppm")?, width, height);
     writer.write_header()?;
 
+    let lower_left_corner = Vec3::new(-2., -1., -1.);
+    let horizontal = Vec3::new(4., 0., 0.);
+    let vertical = Vec3::new(0., 2., 0.);
+    let camera = Vec3::new(0., 0., 0.);
+
     for j in (0..height).rev() {
         for i in 0..width {
-            let r = i as f64 / width as f64;
-            let g = j as f64 / height as f64;
-            let b = 0.2 as f64;
-            writer.write(&Vec3 { x: r, y: g, z: b })?;
+            let u = i as f64 / width as f64;
+            let v = j as f64 / height as f64;
+            writer.write(&ray_color(&Ray::new(
+                        camera.clone(),
+                        &lower_left_corner + u * &horizontal + v * &vertical)))?;
         }
     }
 
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,91 @@
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Vec3 {
     pub x: f64,
     pub y: f64,
     pub z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn len(&self) -> f64 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    pub fn unit(&self) -> Self {
+        self / self.len()
+    }
+}
+
+impl std::ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::Add<Vec3> for &Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
+impl std::ops::Mul<&Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
+impl std::ops::Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Vec3 {
+        (1. / rhs) * self
+    }
+}
+
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn at(&self, t: f64) -> Vec3 {
+        &self.origin + t * &self.direction
+    }
 }
