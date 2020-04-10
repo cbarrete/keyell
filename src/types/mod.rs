7bdbbb869850f769a26542dfef8bc91fe38bf9c8
Cédric Barreteau <cbarrete@users.noreply.github.com>
2020-04-10 15:32:49 +0200

    Don't expose `types` modules

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,8 +1,8 @@
-pub mod vec3;
+mod vec3;
 pub use vec3::Vec3;
-pub mod ray;
+mod ray;
 pub use ray::Ray;
-pub mod hittable;
+mod hittable;
 pub use hittable::{Hit, Hittable};
-pub mod sphere;
+mod sphere;
 pub use sphere::{Sphere};
