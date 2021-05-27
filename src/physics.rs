cf2a37d74ee9b29e8a954e0a762676caa8819bbd
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-05-27 14:06:17 -0400

    Physics: add refraction

--- src/physics.rs
+++ src/physics.rs
@@ -5,6 +5,15 @@ pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
     v - (2. * dot(v, n) * n)
 }
 
+// TODO document and test
+pub fn refract(v: &Vec3, n: &Vec3, n_ratio: f64) -> Vec3 {
+    let cos_theta = dot(&-v, n);
+    let out_perp = n_ratio * (v + cos_theta * n);
+    let len_squared = out_perp.x.powi(2) + out_perp.y.powi(2) + out_perp.z.powi(2);
+    let out_parallel = -(1.0 - len_squared).abs().sqrt() * n;
+    out_perp + out_parallel
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
