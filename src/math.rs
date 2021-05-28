924913314f23fcfdbc6f4925539ed9f09e6775d5
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-05-28 14:16:40 -0400

    Remove gcd function

--- src/math.rs
+++ src/math.rs
@@ -6,15 +6,6 @@ pub fn dot(v1: &Vec3, v2: &Vec3) -> f64 {
     v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
 }
 
-pub fn gcd(mut x: usize, mut y: usize) -> usize {
-    while y != 0 {
-        let tmp = x;
-        x = y;
-        y = tmp % y;
-    }
-    x
-}
-
 pub fn deg_to_radians(d: f64) -> f64 {
     (PI * d) / 180.
 }
