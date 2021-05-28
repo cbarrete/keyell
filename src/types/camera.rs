dd03bb7453f15981c8078760a2b3c2d8da61baae
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-05-28 11:04:54 -0400

    Don't clone the camera's corner vector

--- src/types/camera.rs
+++ src/types/camera.rs
@@ -3,7 +3,7 @@ use crate::types::{Canvas, Point, Ray, Vec3};
 
 #[derive(Debug)]
 pub struct Camera {
-    lower_left_corner: Vec3,
+    to_lower_left_corner: Vec3,
     horizontal: Vec3,
     vertical: Vec3,
     origin: Point,
@@ -15,7 +15,7 @@ impl Camera {
         let h = (canvas.width / gcd) as f64;
         let v = (canvas.height / gcd) as f64;
         Camera {
-            lower_left_corner: Vec3::new(-h / 2., -1., -v / 2.),
+            to_lower_left_corner: Vec3::new(-h / 2., -1., -v / 2.),
             horizontal: Vec3::new(h, 0., 0.),
             vertical: Vec3::new(0., 0., v),
             origin: Point::new(0., 0., 0.),
@@ -25,7 +25,7 @@ impl Camera {
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         Ray {
             origin: self.origin.clone(),
-            direction: self.lower_left_corner.clone() + u * &self.horizontal + v * &self.vertical,
+            direction: &self.to_lower_left_corner + u * &self.horizontal + v * &self.vertical,
         }
     }
 }
