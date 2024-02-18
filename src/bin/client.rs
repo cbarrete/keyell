70c88792426ef79eb83c76b706c1f194f1d01696
Cédric Barreteau <cbarrete@users.noreply.github.com>
2024-02-17 19:11:41 -0500

    Split out client logic into net

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,19 +1,11 @@
-use std::{
-    convert::TryFrom,
-    fs::File,
-    io::{BufWriter, Read, Write},
-    net::TcpStream,
-    ops::Range,
-    sync::Arc,
-};
+use std::{convert::TryFrom, fs::File, io::BufWriter, sync::Arc};
 
 use keyell::{
-    net::Request,
+    net::render_scene_distributed,
     render::{Background, Camera, Canvas, Color, Colorer, Material, Sphere},
     types::Point,
     Scene,
 };
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
 
 fn main() -> std::io::Result<()> {
     let mut scene = Scene {
@@ -46,50 +38,17 @@ fn main() -> std::io::Result<()> {
     ));
 
     let mut pixels = vec![0u8; 3 * canvas.width * canvas.height];
-
-    struct RequestParams<'a> {
-        ip: &'a str,
-        range: Range<usize>,
-        pixels: &'a mut [u8],
-    }
-
-    let cutoff = 800;
-    let (first, second) = pixels.split_at_mut(3 * cutoff * canvas.width);
-    let mut params = [
-        RequestParams {
-            ip: "192.168.1.129:3544",
-            range: 0..cutoff,
-            pixels: first,
-        },
-        RequestParams {
-            ip: "127.0.0.1:3544",
-            range: cutoff..(canvas.height),
-            pixels: second,
-        },
-    ];
-
     let scene = Arc::new(scene);
-    params.par_iter_mut().enumerate().for_each(|(i, params)| {
-        let mut stream = TcpStream::connect(params.ip).unwrap();
-
-        let request = Request {
-            scene: scene.clone(),
-            canvas: canvas.clone(),
-            camera: camera.clone(),
-            samples_per_pixel: 100,
-            maximum_bounces: 10,
-            range: params.range.clone(),
-        };
-
-        let mut serialized = Vec::new();
-        serde_json::to_writer(&mut serialized, &request).unwrap();
-        stream.write(&serialized.len().to_le_bytes()).unwrap();
-        stream.write_all(&serialized).unwrap();
-        stream.flush().unwrap();
-        println!("wrote request {i}");
-        stream.read_exact(params.pixels).unwrap();
-        println!("got response {i}");
-    });
+    render_scene_distributed(
+        ("127.0.0.1:3544", "192.168.1.129:3544"),
+        canvas.height / 4,
+        &mut pixels,
+        scene,
+        canvas.clone(),
+        camera,
+        100,
+        10,
+    );
 
     let mut writer =
         keyell::ppm::PpmWriter::new(BufWriter::new(File::create("client.ppm")?), &canvas);
--- src/net.rs
+++ src/net.rs
@@ -1,5 +1,11 @@
-use std::{ops::Range, sync::Arc};
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+    ops::Range,
+    sync::Arc,
+};
 
+use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -15,4 +21,59 @@ pub struct Request {
     pub samples_per_pixel: usize,
     pub maximum_bounces: usize,
     pub range: Range<usize>,
+}
+
+pub fn render_scene_distributed(
+    ips: (&str, &str),
+    cutoff: usize,
+    pixels: &mut [u8],
+    scene: Arc<Scene>,
+    canvas: Arc<Canvas>,
+    camera: Arc<Camera>,
+    samples_per_pixel: usize,
+    maximum_bounces: usize,
+) {
+    debug_assert!((0..(canvas.height)).contains(&cutoff));
+
+    struct RequestParams<'a> {
+        ip: &'a str,
+        range: Range<usize>,
+        pixels: &'a mut [u8],
+    }
+
+    let (first, second) = pixels.split_at_mut(3 * cutoff * canvas.width);
+    let mut params = [
+        RequestParams {
+            ip: ips.0,
+            range: 0..cutoff,
+            pixels: first,
+        },
+        RequestParams {
+            ip: ips.1,
+            range: cutoff..(canvas.height),
+            pixels: second,
+        },
+    ];
+
+    params.par_iter_mut().enumerate().for_each(|(i, params)| {
+        let mut stream = TcpStream::connect(params.ip).unwrap();
+
+        let request = Request {
+            scene: scene.clone(),
+            canvas: canvas.clone(),
+            camera: camera.clone(),
+            samples_per_pixel,
+            maximum_bounces,
+            range: params.range.clone(),
+        };
+
+        let mut serialized = Vec::new();
+        serde_json::to_writer(&mut serialized, &request).unwrap();
+        stream.write(&serialized.len().to_le_bytes()).unwrap();
+        stream.write_all(&serialized).unwrap();
+        stream.flush().unwrap();
+        println!("wrote request {i}");
+        stream.read_exact(params.pixels).unwrap();
+        println!("got response {i}");
+    });
 }
